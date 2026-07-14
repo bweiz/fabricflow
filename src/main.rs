86ff6c8951f83405127bc0457ba318ad8b591d14
bweiz <ben.weizenegger@gmail.com>
2026-07-13 23:43:43 -0600

    Rust language learning, and initial test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+
+struct MatrixMultiply {
+    m: usize, 
+    n: usize,
+    k: usize
+}
+
+impl MatrixMultiply {
+    fn total_macs(&self) -> usize {
+        self.m * self.n * self.k
+    }
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let workload = MatrixMultiply {
+        m: 8,
+        n: 8,
+        k: 8,
+    };
+
+    println!(
+        "Matrix Multiplication: M={}, N={}, K={}",
+        workload.m,
+        workload.n,
+        workload.k
+    );
+
+    println!("Total MACs: {}", workload.total_macs());
+}
+
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn matrix_multiply_calculates_total_macs() {
+        let workload = MatrixMultiply {
+            m: 8,
+            n: 8,
+            k: 8,
+        };
+        
+        assert_eq!(workload.total_macs(), 512);
+    }
 }
